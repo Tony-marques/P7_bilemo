2caf41f329ead299753504e14cde6fea2131ba09
tony <tony.marques@live.fr>
2024-01-05 18:19:28 +0100

    ✨ Create getProducts

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,19 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\ProductRepository;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Component\Routing\Annotation\Route;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Response;
 
 #[Route(path: "/api", name: "api_")]
-class ProductController extends AbstractController
+class ProductController extends AbstractFOSRestController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): JsonResponse
+    public function __construct(private ProductRepository $productRepository)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+    }
+
+    #[Rest\Get('/products', name: 'get_products')]
+    public function getProducts(): Response
+    {
+        $products = $this->productRepository->findAll();
+
+        $view = $this->view($products, Response::HTTP_OK);
+
+        return $this->handleView($view);
     }
 }
